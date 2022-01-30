471c9d038749ed0d73c1b6d6d946c8001644fb79
teo <karamitrosteo@gmail.com>
2022-01-31 00:24:58 +0200

    Builded Row Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>Hey</h1>
+      {/* Nav */}
+      {/* Banner */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
-}
+};
 
 export default App;
