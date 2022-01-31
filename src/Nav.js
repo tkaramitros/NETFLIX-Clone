32a411f9c761df6dfa9aedfc1a333d1da27b4211
teo <karamitrosteo@gmail.com>
2022-01-31 21:24:01 +0200

    Builded the NavBar

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Netflix_2014_logo.svg"
+        alt="Netflix logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix avatar"
+      />
+    </div>
+  );
+};
+
+export default Nav;
